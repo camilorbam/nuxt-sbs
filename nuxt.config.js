584c9838b9608d5e64707b1613bf0e2506fde626
Camilo Rodriguez Sanchez <camilor@bornagainmedia.com>
2021-03-07 11:03:06 -0500

    Read sbs posts

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,10 @@ export default {
     '@nuxtjs/vuetify',
   ],
 
+  eslint: {
+    fix: true
+  },
+
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
